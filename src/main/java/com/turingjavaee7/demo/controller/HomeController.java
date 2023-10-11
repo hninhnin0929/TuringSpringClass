7477aa5e29da025040081bc00c88ac4732f7810b
hninhnin0929 <hninhnin0929>
2023-10-11 10:04:04 +0900

    Day-6(12-09-23) Introduction to Spring-Spring Core-3

--- src/main/java/com/turingjavaee7/demo/controller/HomeController.java
+++ src/main/java/com/turingjavaee7/demo/controller/HomeController.java
@@ -7,6 +7,7 @@ import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import com.turingjavaee7.demo.model.Store;
 import com.turingjavaee7.demo.service.ArithmeticService;
@@ -72,6 +73,13 @@ public class HomeController {
 			
 		}
 		return "home";
+	}
+	
+	@GetMapping("/search")
+	public String search(@RequestParam String query) {
+		
+		log.info("Query " + query);
+		return "scopesExample";
 	}
 	
 	@GetMapping("/scopes/request")
