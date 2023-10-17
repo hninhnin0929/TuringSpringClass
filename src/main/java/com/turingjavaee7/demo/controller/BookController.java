977bcd3a18a36000ed2c8472294a0c1eee38bc65
hninhnin0929 <hninhnin0929>
2023-10-17 17:30:11 +0900

    Day-9(23-09-23) Spring MVC-3-Controller

--- src/main/java/com/turingjavaee7/demo/controller/BookController.java
+++ src/main/java/com/turingjavaee7/demo/controller/BookController.java
@@ -7,6 +7,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -23,6 +24,14 @@ public class BookController {
 	@Autowired
 	BookService bookService;
 	
+	@ModelAttribute
+	void addEmptyBook(Model model) {
+		log.info("Add Empty Book");
+		List<Book> books = new ArrayList<Book>();
+		books.add(new Book("3", "Title 3", "Author 3 "));
+		model.addAttribute("books", books);
+	}
+	
 	@GetMapping
 	String getAllBook(Model model) {
 		
@@ -32,11 +41,17 @@ public class BookController {
 	}
 	
 	@GetMapping("/{id}")
-	String getBookById(Model model, @PathVariable String id) {
+	String getBookById(Model model, 
+			@PathVariable String id, 
+			@ModelAttribute("books") List<Book> modelBooks) {
 		
+//		List<Book> modelBooks = (ArrayList<Book>)model.getAttribute("books");
+		log.info("Get book by Idc", modelBooks.size());
 		Book book = this.bookService.getBookById(id);
+		
 		List<Book> books = new ArrayList<Book>();
 		books.add(book);
+		
 		model.addAttribute("books", books);
 		return "books/book";
 	}
