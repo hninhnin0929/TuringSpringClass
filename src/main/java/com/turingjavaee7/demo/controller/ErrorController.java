61ac653e8765f0eadbd82c58cd5cc74717fa6508
hninhnin0929 <hninhnin0929>
2023-10-25 17:56:49 +0900

    Day-11(26-09-23) Spring MVC-4-Cross cutting concern

--- src/main/java/com/turingjavaee7/demo/controller/ErrorController.java
+++ src/main/java/com/turingjavaee7/demo/controller/ErrorController.java
@@ -1,5 +1,23 @@
 package com.turingjavaee7.demo.controller;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.SessionAttributes;
+
+import lombok.extern.slf4j.Slf4j;
+
+@Slf4j
+@Controller
+@RequestMapping("/error")
 public class ErrorController {
+	
+	@GetMapping
+	String error(Model model)
+	{
+		log.info("We go error ");
+		return "/error";
+	}
 
 }
